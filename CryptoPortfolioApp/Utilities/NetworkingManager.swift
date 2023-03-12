72bab322e21de702dd39b49de78a3b2f3629f7f0
Leandro Tamola <leatamola@gmail.com>
2023-03-12 20:52:47 -0300

    added reusable networking layer

---
+++ CryptoPortfolioApp/Utilities/NetworkingManager.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkingManager.swift
+//  CryptoPortfolioApp
+//
+//  Created by Leandro Támola on 12/03/2023.
+//
+
+import Combine
+import Foundation
+
+final class NetworkingManager {
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown(url: URL)
+
+        var errorDescription: String? {
+            switch self {
+            case let .badURLResponse(url: url): return "[🔥] Bad response from URL. \(url)"
+            case let .unknown(url: url): return "[⚠️] Unknown error occured. \(url)"
+            }
+        }
+    }
+
+    static func download(url: URL) -> AnyPublisher<Data, any Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else { throw NetworkingError.badURLResponse(url: url) }
+        return output.data
+    }
+
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case let .failure(error):
+            print(error.localizedDescription)
+        }
+    }
+}
